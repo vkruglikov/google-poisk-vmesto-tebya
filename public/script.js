a3c1a2d4d2bb65d131881d81773e7159bb7dfa5f
Valentin Kruglikov <dev.n@bk.ru>
2023-11-15 12:44:45 +0700

    form

--- public/script.js
+++ public/script.js
@@ -1,25 +1,80 @@
+function fallbackCopyTextToClipboard(text) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        const successful = document.execCommand('copy');
+        const msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).catch(console.log);
+}
+
 function App() {
     this.dom = {
         input: document.getElementById('input'),
         form: document.getElementById('form'),
         buttonSubmit: document.getElementById('submit'),
         mouse: document.getElementById('mouse'),
+        linkResult: document.getElementById('link-result'),
+        linkResultCopy: document.getElementById('link-result-copy'),
     };
-    this.searchQuery = (new URLSearchParams(window.location.search)).get('q');
-
+    this.isPlayMode = false;
     this.dom.form.addEventListener('submit', (e) => {
         e.preventDefault();
         this.onSubmit();
     })
+    this.play(
+        (new URLSearchParams(window.location.search)).get('q')
+    );
 
-    this.play(this.searchQuery);
+    if (!this.isPlayMode) {
+        this.dom.input.focus();
+
+        this.dom.linkResultCopy.addEventListener('click', (e) => {
+            e.preventDefault();
+            copyTextToClipboard(e.target.getAttribute('href'));
+        });
+    }
 }
 App.prototype = {
     onSubmit() {
-        console.log('Отправлено')
+        if (this.isPlayMode) return;
+
+        const value = (this.dom.input.value || '').trim();
+        if (!value) return;
+        this.dom.linkResult.classList.remove('hidden');
+
+        for (let link of this.dom.linkResult.querySelectorAll('a')) {
+            link.setAttribute('href', encodeURI("https://google-poisk-vmesto-tebya.ru/?q=" + this.dom.input.value));
+        }
     },
     play(str) {
         if (!str) return;
+        this.isPlayMode = true;
+
+        document.body.classList.add('play-mode');
+
+        this.dom.input.setAttribute('disabled', 'disabled');
         this.dom.input.value = '';
 
         this.playFocusToInput(() => {
@@ -30,7 +85,7 @@ App.prototype = {
                         this.delay(() =>
                             this.playSubmit(() => {
                                 this.delay(() => {
-                                    window.location.href = 'https://google.com/search?q=' + this.searchQuery;
+                                    window.location.href = encodeURI("https://google-poisk-vmesto-tebya.ru/?q=" + str);
                                 })
                             })
                         );
@@ -49,7 +104,7 @@ App.prototype = {
         this.dom.mouse.style.transform = 'translate(' + (position.left  + offset.left)+ 'px, ' + (position.top + offset.top) + 'px)';
         setTimeout(() => {
             callback();
-        }, 2000);
+        }, 1000);
     },
     playInputText(string, callback) {
         const letters = string.trim().split('');
@@ -60,7 +115,7 @@ App.prototype = {
                 return;
             }
             this.dom.input.value += letters.shift();
-        }, 250);
+        }, 150);
     },
     playSubmit(callback) {
         const position = this.dom.buttonSubmit.getBoundingClientRect();
@@ -75,7 +130,7 @@ App.prototype = {
         }, 2000);
     },
     delay(callback, time) {
-        setTimeout(() => callback(), time || 1000);
+        setTimeout(() => callback(), time || 500);
     }
 }
 
